c8cce4832baef4c7cea8483d139f4a7fb9915dc1
Andrey <antonov.andrey.10101@gmail.com>
2023-09-14 11:22:18 +0300

    feat: add card to App

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,46 @@
+
+
 <template>
-  <div>Hello</div>
+  <main>
+    <div v-show="!isVisible" class="card-container">
+      <div class="content-container">
+        <div class="star-icon">
+          <img src="./assets/images/icon-star.svg" alt="" />
+        </div>
+        <div class="text">
+          <h1>How did we do?</h1>
+          <p>
+            Please let us know how we did with your support request. All
+            feedback is appreciated to help us improve our offering!
+          </p>
+        </div>
+
+        <ul>
+          <li
+            v-for="rate in rates"
+            :key="rate"
+            @click="activeRef = rate"
+            class="rating"
+            :class="{ active: activeRef === rate }"
+          >
+            {{ rate }}
+          </li>
+        </ul>
+
+        <button @click="isVisible = !isVisible" class="btn">Submit</button>
+      </div>
+    </div>
+  </main>
 </template>
 
-<script lang="ts">
-export default {
-  setup() {
-    return {};
-  },
-};
+<script setup>
+import { ref } from "vue";
+
+const isVisible = ref(false);
+const activeRef = ref(null);
+const rates = ref([1, 2, 3, 4, 5]);
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+@import './style';
 </style>
